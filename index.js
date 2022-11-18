8611085ee992b0761b8d12ab9be96154192a62d9
Matus Zeman <matus.zeman@gmail.com>
2022-11-18 23:00:19 +0100

    feat: memory simulations

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+const {Buffer} = require('node:buffer');
 const Koa = require('koa');
-const Router = require('koa-router');
+const Router = require('@koa/router');
 const os = require('os');
 const fs = require('fs');
 const globby = require('globby');
 
-const config = {
+const appConfig = {
   koa_proxy: !!Number(process.env.koa_proxy),
-  process_sigterm_sleep: Number(process.env.process_sigterm_sleep)
+  process_sigterm_sleep: process.env.process_sigterm_sleep ? Number(process.env.process_sigterm_sleep) : 0,
+  volume_path: process.env.process_sigterm_sleep ?? __dirname + '/data'
 }
 
-console.log('Config:', JSON.stringify(config, null, 2))
+console.log('Config:', JSON.stringify(appConfig, null, 2))
+console.log('Garbage collector active: ', !!global.gc);
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 process.on('SIGTERM', () => {
   console.log('SIGTERM received.');
@@ -19,9 +26,9 @@ process.on('SIGTERM', () => {
     process.exit(0);
   }
 
-  if (config.process_sigterm_sleep) {
-    console.log(`Sleep ${config.process_sigterm_sleep}ms before exiting the process (config:process_sigterm_sleep)`, config.process_sigterm_sleep);
-    setTimeout(exit, config.process_sigterm_sleep);
+  if (appConfig.process_sigterm_sleep) {
+    console.log(`Sleep ${appConfig.process_sigterm_sleep}ms before exiting the process (config:process_sigterm_sleep)`, appConfig.process_sigterm_sleep);
+    setTimeout(exit, appConfig.process_sigterm_sleep);
     return;
   }
 
@@ -30,18 +37,19 @@ process.on('SIGTERM', () => {
 
 const app = new Koa({
   // https://github.com/koajs/koa/blob/master/docs/api/request.md#requestips
-  proxy: config.koa_proxy
+  proxy: appConfig.koa_proxy
 });
+
 const router = new Router();
 
 const startupTime = Date.now();
+const startupMemoryUsage = process.memoryUsage().rss;
 
 router.get('/', async ctx => {
   ctx.body = {
     runningTime: Math.ceil((Date.now() - startupTime) / 1000),
     hostname: os.hostname(),
-    arch: os.arch(),
-    freemem: os.freemem()
+    arch: os.arch()
   };
 });
 
@@ -68,31 +76,98 @@ router.get('/network', async ctx => {
 });
 
 router.get('/volume', async ctx => {
-  var path = __dirname + '/data';
-  if (ctx.request.query.file) {
-    path += `/${ctx.request.query.file}`;
+  const files = await globby(['**/*'], {
+    cwd: appConfig.volume_path
+  });
+  ctx.body = {
+    files
+  };
+});
+
+const memoryBuffers = {
+  extraMemory: Buffer.alloc(0, 1, 'binary')
+}
+
+router.get('/memory/set', async ctx => {
+  const options = {
+    megabytes: Number(ctx.query.megabytes),
+  }
+  console.log('/memory/set', options);
+
+  setMemory('extraMemory', options.megabytes)
+
+  ctx.body = {
+    options
+  }
+})
+
+router.get('/memory/inc', async ctx => {
+  const options = {
+    megabytes: Number(ctx.query.megabytes),
+  }
+  console.log('/memory/inc', options);
+
+  incMemory('extraMemory', options.megabytes);
+
+  ctx.body = {
+    options
+  };
+});
+
+router.get('/memory/dec', async ctx => {
+  const options = {
+    megabytes: Number(ctx.query.megabytes),
+  }
+  console.log('/memory/dec', options);
+
+  decMemory('extraMemory', options.megabytes);
+
+  ctx.body = {
+    options
+  };
+});
+
+router.get('/memory/stress', async ctx => {
+  const options = {
+    megabytes: Number(ctx.query.megabytes),
+    sleep_after: ctx.query.sleep_after ? Number(ctx.query.sleep_after) : 0,
+    async: !!ctx.query.async
+  }
+  console.log('/memory/stress', options);
+
+  if (options.async) {
+    incMemory('extraMemory', options.megabytes);
+
+    setTimeout(() => {
+      decMemory('extraMemory', options.megabytes);
+      console.log('/stress async finished', options);
+    }, options.sleep_after * 1000);
+  } else {
+    console.log(`/stress sleep for ${options.sleep_after}s`);
+    await sleep(options.sleep_after * 1000);
+    decMemory('extraMemory', options.megabytes);
   }
 
-  try {
-    if (fs.lstatSync(path).isFile()) {
-      ctx.type = 'text/plain';
-      ctx.body = fs.createReadStream(path);
-      return;
-    }
+  ctx.body = {
+    options
+  };
+});
 
-    const files = await globby(['**/*'], {
-      cwd: __dirname + '/data'
-    });
-    ctx.body = {
-      files
-    };
-  } catch (e) {
+router.get('/volume/(.*)', async ctx => {
+  const path = `${appConfig.volume_path}/${ctx.params[0]}`;
+  const stat = fs.statSync(path, {throwIfNoEntry: false});
+  if (!stat) {
+    ctx.status = 404;
     ctx.body = {
       error: {
-        message: e.message
+        message: 'File does not exists',
+        file: path
       }
     }
+    return;
   }
+  ctx.type = 'text/plain';
+  ctx.body = fs.createReadStream(path);
 });
 
 router.get('/process', async ctx => {
@@ -105,10 +180,88 @@ router.get('/process', async ctx => {
   };
 });
 
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
+app.use(async (ctx, next) => {
+  try {
+    console.log('Request: ', ctx.originalUrl);
+    const before = getProcessStats();
+    await next();
+    if (ctx.body) {
+      ctx.body.request = {
+        before,
+        after: getProcessStats()
+      };
+    }
+  } catch(e) {
+    console.error(e);
+    ctx.status = 500;
+    ctx.body = {
+      error: {
+        message: e.message,
+        code: e.code,
+        stack: e.stack
+      }
+    }
+  }
+});
+
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(3000);
+
+function setMemory(buffer, megabytes) {
+  global[buffer] = null;
+  gc();
+  global[buffer] = Buffer.alloc(mbsToBytes(megabytes), 1, 'binary');
+}
+
+function incMemory(buffer, megabytes) {
+  const prevSize = memoryBuffers[buffer].length;
+  const bytes = mbsToBytes(megabytes);
+  memoryBuffers[buffer] = null;
+  gc();
+  memoryBuffers[buffer] = Buffer.alloc(bytes + prevSize, 1, 'binary');
+}
+
+function decMemory(buffer, megabytes) {
+  const prevSize = memoryBuffers[buffer].length;
+  const bytes = mbsToBytes(megabytes);
+  memoryBuffers[buffer] = null;
+  gc();
+  let requestedSize = prevSize - bytes;
+  if (requestedSize < 0) {
+    requestedSize = 0;
+  }
+  memoryBuffers[buffer] = Buffer.alloc(requestedSize, 1, 'binary');
+}
+
+function gc() {
+  if (global.gc) {
+    global.gc();
+  }
+}
+
+function getProcessStats() {
+  gc();
+  const memory = process.memoryUsage();
+  return {
+    startupMemoryUsage: bytesToMbs(startupMemoryUsage),
+    freeMemory: bytesToMbs(os.freemem()),
+    rss: bytesToMbs(memory.rss),
+    heapTotal: bytesToMbs(memory.heapTotal),
+    heapUsed: bytesToMbs(memory.heapUsed),
+    external: bytesToMbs(memory.external),
+    extraMemory: bytesToMbs(memoryBuffers['extraMemory'].length)
+  }
+}
+
+const bytesInMb = 1048576;
+
+function bytesToMbs(bytes) {
+  return Math.ceil(bytes / bytesInMb);
+}
+
+function mbsToBytes(megabytes) {
+  return megabytes * bytesInMb;
+}
 
 console.log('Running on http://localhost:3000/');
